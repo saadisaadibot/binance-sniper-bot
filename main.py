d551e80ccbe58bd4a2d53d0bf090ecfe42cf3c5f
saadisaadibot <alhossiniabdulhalim3@gmail.com>
2025-07-25 01:19:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
-import os
-import requests
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-def send_message(chat_id, text):
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    payload = {
-        "chat_id": chat_id,
-        "text": text
-    }
-    requests.post(url, json=payload)
 
 @app.route('/', methods=['POST'])
 def telegram_webhook():
     data = request.json
-    chat_id = data['message']['chat']['id']
-    message = data['message']['text']
-    print(f"📩 Received message: {message}")
-    print(f"💬 Chat ID: {chat_id}")
-    send_message(chat_id, f"📬 وصلتني رسالتك: {message}")
+    if data.get("message"):
+        chat_id = data['message']['chat']['id']
+        message = data['message'].get('text', '')
+        print(f"💬 Received message: {message}")
+        print(f"👤 Chat ID: {chat_id}")
     return "ok"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
