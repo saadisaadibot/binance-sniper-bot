5fe88a4bb55fc82ccec85d183bd45cbffa3116c0
saadisaadibot <alhossiniabdulhalim3@gmail.com>
2025-08-16 14:33:47 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,4 @@
-import sys, traceback
-try:
-    from main import app
-except Exception as e:
-    print("WSGI_IMPORT_ERROR:", repr(e), file=sys.stderr)
-    traceback.print_exc()
-    raise
+from main import app
+
+if __name__ == "__main__":
+    app.run()
