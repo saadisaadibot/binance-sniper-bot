417b2a651624a2323108ce6e87e842b2e7563144
saadisaadibot <alhossiniabdulhalim3@gmail.com>
2025-08-16 14:29:26 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,6 @@
-# wsgi.py
 import sys, traceback
-
 try:
-    from main import app  # main = اسم الملف main.py
+    from main import app
 except Exception as e:
     print("WSGI_IMPORT_ERROR:", repr(e), file=sys.stderr)
     traceback.print_exc()
